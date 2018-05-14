d4242e8829afcb772ab98d0c0f596e7297d177cf
wcavell <wcavell@LIVE.COM>
2018-05-14 09:55:10 +0800

    GX

--- CoreAnimation/CALayer.cs
+++ CoreAnimation/CALayer.cs
@@ -5,10 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Geometry;
 
 namespace CoreAnimation
 { 
-    public class CALayer:DependencyObject
+    public abstract class CALayer
     {
+        internal void SetDrawer(CanvasDrawingSession draw)
+        {
+            Drawer = draw;
+        }
+        protected CanvasDrawingSession Drawer;
+        public List<CALayer> Layers { get; set; } = new List<CALayer>();
+        public virtual void Draw()
+        {
+            
+        }
+
+        public void AddSubLayer(CALayer layer)
+        {
+            Layers.Add(layer);
+        }
+
+        public void InsertSubLayer(CALayer layer,int index)
+        {
+            Layers.Insert(index,layer);
+        }
+        public void RemoveLayer(CALayer layer)
+        {
+            Layers.Remove(layer);
+        }
+
+        public void RemoveLayer(int index)
+        {
+            if (Layers.Count > index)
+            {
+                Layers.RemoveAt(index);
+            }
+        }
     }
 }
